cd618fa091c3ae17275639e90e2060526ece4b52
HighHarmonics <highharmonics@gmail.com>
2024-02-10 10:48:01 -0800

    Update EventHeader.tsx
    formatting

--- src/app/components/EventHeader.tsx
+++ src/app/components/EventHeader.tsx
@@ -33,10 +33,9 @@ function EventHeader({event, reservedStreamCount, totalStreamCount}:Props): Reac
             <img src={logo} className="App-logo" alt="logo" />
             <h4>TOPLAP Presents ...</h4>
             <h1>TOPLAP20 Live Stream</h1>
-             <b>Note:</b> This is event is not yet open for slot registration.
-             Slot registration opens Monday, Feb 12th. 
             <h2> {event && DateTime.fromISO(event.starts_at).toFormat("dd. LLLL HH:mm")} - {event && DateTime.fromISO(event.ends_at).toFormat("dd. LLLL HH:mm")} {event && DateTime.fromISO(event.starts_at).toFormat("yyyy")}</h2>
-           
+             This is event is not yet open for registration.
+             <br>Slot registration opens Friday, Feb 16th. </br>
             <hr/>
 
             <ProgressBar
